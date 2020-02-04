66a5fc19d09661b0cd852bd5a2ea4301f1b8dde6
howdym <marcjiang312@gmail.com>
2020-02-03 18:47:42 -0600

    New version

--- mass_processing.py
+++ mass_processing.py
@@ -2,10 +2,8 @@ import video_processing
 import os
 
 directory_start = "./"
+situation = input("What are you looking for (code_builder or portfolio): ")
 
 for filename in os.listdir(directory_start):
     if filename.endswith(".mp4") or filename.endswith(".avi"):
-        video = os.path.join(directory_start, filename)
-        logname = filename[0:len(filename) - 4]
-        name = logname + "_ci-instances.txt"
-        video_processing.process_video(video, name)
+        video_processing.process_video(filename, situation)
--- video_processing.py
+++ video_processing.py
@@ -1,28 +1,64 @@
-import image_processing
-import analyze
+from __future__ import print_function
+import cv2 as cv
+import os
+import util
+import moviepy.editor
+
+# "C:\\Users\\marcj\\Desktop\\Github Projects\\MinecraftCV\\mc07.mp4"
 
 
-def process_video(mov, name):
-    movie = mov
-    imgdir = 'frames'
-    times = image_processing.make_list(movie)
-    temp_times = []
+def process_video(mov, situation):
+    print(mov)
+    cap = cv.VideoCapture(mov)
+    frame_counter = 0
+    snip = False
+    running = 0
+    begin = 0
+    end = 0
+    clip = moviepy.editor.VideoFileClip(mov)
+    os.mkdir("Yes_" + mov[:-4])
+    os.mkdir("No_" + mov[:-4])
+    os.mkdir("Videos_" + mov[:-4])
 
-    for time in times:
-        path = image_processing.extract_frames(movie, time, imgdir)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
 
-        match = analyze.analyzation("./target.png", path)
+            total = False
+            if situation == "code_builder":
+                tr = util.confirm_top_bar(frame)
+                tr1 = util.confirm_lower_bar(frame)
+                tr2 = util.confirm_side_bar(frame)
+                total = tr or tr1 or tr2
+            elif situation == "portfolio":
+                total = util.confirm_portfolio(frame)
 
-        if match:
-            temp_times.append(time)
+            if total:
+                imgpath = os.path.join("Yes_" + mov[:-4], '{}.png'.format(frame_counter))
+                cv.imwrite(imgpath, frame)
+                if snip:
+                    end += 0.2
+                else:
+                    snip = True
+                    begin = running
+                    end = begin
+            else:
+                imgpath = os.path.join("No_" + mov[:-4], '{}.png'.format(frame_counter))
+                cv.imwrite(imgpath, frame)
+                if snip:
+                    temp = clip.subclip(begin, end)
+                    temp.write_videofile(os.path.join("Videos_" + mov[:-4], "_{}_{}.mp4".format(round(begin),
+                                                                                               int(end + 1))))
+                    snip = False
+            running += 0.2
+            running = round(running, 1)
+            print(frame_counter)
+            frame_counter += 1
+            continue
 
-        if image_processing.check_folder(imgdir):
-            image_processing.clear_folder(imgdir)
-
-    times = temp_times
-
-    # for time in times:
-    #     image_processing.extract_frames(movie, time, imgdir)
-
-    result = analyze.parse_list(times)
-    analyze.write_results(result, name)
+        else:
+            if snip:
+                temp = clip.subclip(begin, end)
+                temp.write_videofile(os.path.join("Videos_" + mov[:-4], "_{}_{}.mp4".format(round(begin),
+                                                                                           int(end + 1))))
+            break
