7bd1792b73c46cb17845ab0dfc9b3c0001e79a64
Marc Jiang <marcjiang312@gmail.com>
2020-09-08 01:55:59 -0500

    Revamping

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,4 @@
-import os
-from moviepy.editor import *
-import numpy
+import moviepy.editor
 
 
 def close_clip(clip):
@@ -13,27 +11,3 @@ def close_clip(clip):
         del clip.audio
     del clip
 
-
-def extract_frames(movie, time, imgdir):
-    clip = VideoFileClip(movie)
-    imgpath = os.path.join(imgdir, '{}.png'.format(time))
-    clip.save_frame(imgpath, time)
-    close_clip(clip)
-    return imgpath
-
-
-def make_list(movie):
-    clip = VideoFileClip(movie)
-    length = clip.duration
-    frame_list = numpy.arange(0,length, 0.25)
-    close_clip(clip)
-    return frame_list
-
-
-def check_folder(imgdir):
-    return len(os.listdir(imgdir)) >= 5
-
-
-def clear_folder(imgdir):
-    for filename in os.listdir(imgdir):
-        os.system("del /f " + os.path.join(imgdir, filename))
